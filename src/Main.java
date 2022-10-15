8711e8b508ecccbfac6a779c5bb794ca280256c3
Us3rL0sT <akshencev04@mail.ru>
2022-10-15 17:31:12 +0500

    fix_warnings

--- src/Main.java
+++ src/Main.java
@@ -32,129 +32,131 @@ public class Main {
         String input1 = words[0]; // \\
         String action = words[1]; // -- -- запись из массива в переменные
         String input2 = words[2]; // //
-        byte number1 = 0;
-        byte number2 = 0;
+        byte number1;
+        byte number2;
         boolean rim1;
         boolean rim2;
-        switch (input1){
-            case ("I"):
+        switch (input1) {
+            case ("I") -> {
                 number1 = 1;
                 rim1 = true;
-                break;
-            case ("II"):
+            }
+            case ("II") -> {
                 number1 = 2;
                 rim1 = true;
-                break;
-            case ("III"):
+            }
+            case ("III") -> {
                 number1 = 3;
                 rim1 = true;
-                break;
-            case ("IV"):
+            }
+            case ("IV") -> {
                 number1 = 4;
                 rim1 = true;
-                break;
-            case ("V"):
+            }
+            case ("V") -> {
                 number1 = 5;
                 rim1 = true;
-                break;
-            case ("VI"):
+            }
+            case ("VI") -> {
                 number1 = 6;
                 rim1 = true;
-                break;
-            case ("VII"):
+            }
+            case ("VII") -> {
                 number1 = 7;
                 rim1 = true;
-                break;
-            case ("VIII"):
+            }
+            case ("VIII") -> {
                 number1 = 8;
                 rim1 = true;
-                break;
-            case ("IX"):
+            }
+            case ("IX") -> {
                 number1 = 9;
                 rim1 = true;
-                break;
-            case ("X"):
+            }
+            case ("X") -> {
                 number1 = 10;
                 rim1 = true;
-                break;
-            default:
+            }
+            default -> {
                 number1 = Byte.parseByte(input1);
                 rim1 = false;
-                break;
+            }
         } // ввод первого римского числа
 
-        switch (input2){
-            case ("I"):
+        switch (input2) {
+            case ("I") -> {
                 number2 = 1;
                 rim2 = true;
-                break;
-            case ("II"):
+            }
+            case ("II") -> {
                 number2 = 2;
                 rim2 = true;
-                break;
-            case ("III"):
+            }
+            case ("III") -> {
                 number2 = 3;
                 rim2 = true;
-                break;
-            case ("IV"):
+            }
+            case ("IV") -> {
                 number2 = 4;
                 rim2 = true;
-                break;
-            case ("V"):
+            }
+            case ("V") -> {
                 number2 = 5;
                 rim2 = true;
-                break;
-            case ("VI"):
+            }
+            case ("VI") -> {
                 number2 = 6;
                 rim2 = true;
-                break;
-            case ("VII"):
+            }
+            case ("VII") -> {
                 number2 = 7;
                 rim2 = true;
-                break;
-            case ("VIII"):
+            }
+            case ("VIII") -> {
                 number2 = 8;
                 rim2 = true;
-                break;
-            case ("IX"):
+            }
+            case ("IX") -> {
                 number2 = 9;
                 rim2 = true;
-                break;
-            case ("X"):
+            }
+            case ("X") -> {
                 number2 = 10;
                 rim2 = true;
-                break;
-            default:
+            }
+            default -> {
                 number2 = Byte.parseByte(input2);
                 rim2 = false;
-                break;
+            }
         } // ввод второго римского числа
 
         if ((number1 >= 0 && number1 <= 10) && (number2 >= 0 && number2 <= 10)) {
-            if (rim1 == false && rim2 == false) {
+            if (!rim1 && !rim2) {
                 String result;
                 switch (action) {
-                    case ("+"):
+                    case ("+") -> {
                         result = String.valueOf(number1 + number2);
                         System.out.println(result);
                         return result;
-                    case("-"):
+                    }
+                    case ("-") -> {
                         result = String.valueOf(number1 - number2);
                         System.out.println(result);
                         return result;
-                    case("*"):
+                    }
+                    case ("*") -> {
                         result = String.valueOf(number1 * number2);
                         System.out.println(result);
                         return result;
-                    case("/"):
+                    }
+                    case ("/") -> {
                         result = String.valueOf(number1 / number2);
                         System.out.println(result);
                         return result;
-
-
+                    }
                 }
             }
-            else if ((rim1 == true && rim2 == false) || (rim1 == false && rim2 == true)) {
+            else if ((rim1 && !rim2) || (!rim1 && rim2)) {
                 try {
                     throw new Exception();
                 } catch (Exception e) {
@@ -166,68 +168,29 @@ public class Main {
                 switch (action) {
                     case ("+"):
                         result = String.valueOf(number1 + number2);
-                        switch (result){
-                            case ("1"):
-                                result = "I";
-                                break;
-                            case ("2"):
-                                result = "II";
-                                break;
-                            case ("3"):
-                                result = "III";
-                                break;
-                            case ("4"):
-                                result = "IV";
-                                break;
-                            case ("5"):
-                                result = "V";
-                                break;
-                            case ("6"):
-                                result = "VI";
-                                break;
-                            case ("7"):
-                                result = "VII";
-                                break;
-                            case ("8"):
-                                result = "VIII";
-                                break;
-                            case ("9"):
-                                result = "IX";
-                                break;
-                            case ("10"):
-                                result = "X";
-                                break;
-                            case ("11"):
-                                result = "XI";
-                                break;
-                            case ("12"):
-                                result = "XII";
-                                break;
-                            case ("13"):
-                                result = "XIII";
-                                break;
-                            case ("14"):
-                                result = "XIV";
-                                break;
-                            case ("15"):
-                                result = "XV";
-                                break;
-                            case ("16"):
-                                result = "XVI";
-                                break;
-                            case ("17"):
-                                result = "XVII";
-                                break;
-                            case ("18"):
-                                result = "XVIII";
-                                break;
-                            case ("19"):
-                                result = "XIX";
-                                break;
-                            case ("20"):
-                                result = "XX";
-                                break;
-                        }
+                        result = switch (result) {
+                            case ("1") -> "I";
+                            case ("2") -> "II";
+                            case ("3") -> "III";
+                            case ("4") -> "IV";
+                            case ("5") -> "V";
+                            case ("6") -> "VI";
+                            case ("7") -> "VII";
+                            case ("8") -> "VIII";
+                            case ("9") -> "IX";
+                            case ("10") -> "X";
+                            case ("11") -> "XI";
+                            case ("12") -> "XII";
+                            case ("13") -> "XIII";
+                            case ("14") -> "XIV";
+                            case ("15") -> "XV";
+                            case ("16") -> "XVI";
+                            case ("17") -> "XVII";
+                            case ("18") -> "XVIII";
+                            case ("19") -> "XIX";
+                            case ("20") -> "XX";
+                            default -> result;
+                        };
                         System.out.println(result);
                         return result;
                     case("-"):
@@ -243,156 +206,69 @@ public class Main {
                         }
                         result = String.valueOf(number1 - number2);
 
-                        switch (result){
-                            case ("1"):
-                                result = "I";
-                                break;
-                            case ("2"):
-                                result = "II";
-                                break;
-                            case ("3"):
-                                result = "III";
-                                break;
-                            case ("4"):
-                                result = "IV";
-                                break;
-                            case ("5"):
-                                result = "V";
-                                break;
-                            case ("6"):
-                                result = "VI";
-                                break;
-                            case ("7"):
-                                result = "VII";
-                                break;
-                            case ("8"):
-                                result = "VIII";
-                                break;
-                            case ("9"):
-                                result = "IX";
-                                break;
-                        }
+                        result = switch (result) {
+                            case ("1") -> "I";
+                            case ("2") -> "II";
+                            case ("3") -> "III";
+                            case ("4") -> "IV";
+                            case ("5") -> "V";
+                            case ("6") -> "VI";
+                            case ("7") -> "VII";
+                            case ("8") -> "VIII";
+                            case ("9") -> "IX";
+                            default -> result;
+                        };
                         System.out.println(result);
                         return result;
                     case("*"):
                         result = String.valueOf(number1 * number2);
-                        switch (result){ // без понятия как автоматизировать, а до 100 прописывать не хочется)
-                            case ("1"):
-                                result = "I";
-                                break;
-                            case ("2"):
-                                result = "II";
-                                break;
-                            case ("3"):
-                                result = "III";
-                                break;
-                            case ("4"):
-                                result = "IV";
-                                break;
-                            case ("5"):
-                                result = "V";
-                                break;
-                            case ("6"):
-                                result = "VI";
-                                break;
-                            case ("7"):
-                                result = "VII";
-                                break;
-                            case ("8"):
-                                result = "VIII";
-                                break;
-                            case ("9"):
-                                result = "IX";
-                                break;
-                            case ("10"):
-                                result = "X";
-                                break;
-                            case ("11"):
-                                result = "XI";
-                                break;
-                            case ("12"):
-                                result = "XII";
-                                break;
-                            case ("13"):
-                                result = "XIII";
-                                break;
-                            case ("14"):
-                                result = "XIV";
-                                break;
-                            case ("15"):
-                                result = "XV";
-                                break;
-                            case ("16"):
-                                result = "XVI";
-                                break;
-                            case ("17"):
-                                result = "XVII";
-                                break;
-                            case ("18"):
-                                result = "XVIII";
-                                break;
-                            case ("19"):
-                                result = "XIX";
-                                break;
-                            case ("20"):
-                                result = "XX";
-                                break;
-                            case ("21"):
-                                result = "XXI";
-                                break;
-                            case ("24"):
-                                result = "XXIV";
-                                break;
-                            case ("27"):
-                                result = "XXVII";
-                                break;
-                            case ("28"):
-                                result = "XXVIII";
-                                break;
-                            case ("30"):
-                                result = "XXX";
-                                break;
-                            case ("32"):
-                                result = "XXXII";
-                                break;
-                        }
+                        result = switch (result) { // без понятия как автоматизировать, а до 100 прописывать не хочется)
+                            case ("1") -> "I";
+                            case ("2") -> "II";
+                            case ("3") -> "III";
+                            case ("4") -> "IV";
+                            case ("5") -> "V";
+                            case ("6") -> "VI";
+                            case ("7") -> "VII";
+                            case ("8") -> "VIII";
+                            case ("9") -> "IX";
+                            case ("10") -> "X";
+                            case ("11") -> "XI";
+                            case ("12") -> "XII";
+                            case ("13") -> "XIII";
+                            case ("14") -> "XIV";
+                            case ("15") -> "XV";
+                            case ("16") -> "XVI";
+                            case ("17") -> "XVII";
+                            case ("18") -> "XVIII";
+                            case ("19") -> "XIX";
+                            case ("20") -> "XX";
+                            case ("21") -> "XXI";
+                            case ("24") -> "XXIV";
+                            case ("27") -> "XXVII";
+                            case ("28") -> "XXVIII";
+                            case ("30") -> "XXX";
+                            case ("32") -> "XXXII";
+                            default -> result;
+                        };
 
                         System.out.println(result);
                         return result;
                     case("/"):
                         result = String.valueOf(number1 / number2);
-                        switch (result){
-                            case ("1"):
-                                result = "I";
-                                break;
-                            case ("2"):
-                                result = "II";
-                                break;
-                            case ("3"):
-                                result = "III";
-                                break;
-                            case ("4"):
-                                result = "IV";
-                                break;
-                            case ("5"):
-                                result = "V";
-                                break;
-                            case ("6"):
-                                result = "VI";
-                                break;
-                            case ("7"):
-                                result = "VII";
-                                break;
-                            case ("8"):
-                                result = "VIII";
-                                break;
-                            case ("9"):
-                                result = "IX";
-                                break;
-                            case ("10"):
-                                result = "X";
-                                break;
-                        }
+                        result = switch (result) {
+                            case ("1") -> "I";
+                            case ("2") -> "II";
+                            case ("3") -> "III";
+                            case ("4") -> "IV";
+                            case ("5") -> "V";
+                            case ("6") -> "VI";
+                            case ("7") -> "VII";
+                            case ("8") -> "VIII";
+                            case ("9") -> "IX";
+                            case ("10") -> "X";
+                            default -> result;
+                        };
                         System.out.println(result);
                         return result;
                     default:
